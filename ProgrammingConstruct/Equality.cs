72d419e312c940f5a933a8fb1c0326e2f2128104
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-14 10:23:12 +0530

    [Rutuja]:Check Numbers are equal or not

---
+++ ProgrammingConstruct/Equality.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingConstruct
+{
+    internal class Equality
+    {
+        public static void CheckEquality()
+        {
+            Console.WriteLine("Enter first number");
+            int FirstNumber=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int SecondNumber=Convert.ToInt32(Console.ReadLine());
+            if (FirstNumber==SecondNumber)
+            {
+                Console.WriteLine("First Number is Equal to Second Number");
+            }
+            else
+            {
+                Console.WriteLine("Numbers are not Equal");
+            }
+        }
+    }
+}
--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -7,6 +7,7 @@ namespace ProgrammingConstruct
         static void Main(string[] args)
         {
             Console.WriteLine("Basic Problems");
+            Equality.CheckEquality();
         }
     }
 }

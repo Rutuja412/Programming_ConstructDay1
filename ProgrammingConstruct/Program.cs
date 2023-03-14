b7787923f5e8aceb31c89c93e0b14cf055c703ad
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-14 10:14:14 +0530

    [Rutuja]:Welcome

---
+++ ProgrammingConstruct/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgrammingConstruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Problems");
+        }
+    }
+}

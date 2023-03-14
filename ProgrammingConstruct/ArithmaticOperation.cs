ff2471a6dbb6eb5211624137b7d008794dd9f6a2
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-14 17:57:13 +0530

    [Rutuja]:Adding Arithmatic Operation

---
+++ ProgrammingConstruct/ArithmaticOperation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingConstruct
+{
+    internal class ArithmaticOperation
+    {
+        public static void Calculator()
+        {
+            Console.WriteLine("Enter First Number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Select Correct Option");
+            Console.WriteLine("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Exit");
+            int option = Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Additiom : " + (x + y));
+                    break;
+                    case 2:
+                    Console.WriteLine("Substraction :" + (x - y));
+                    break; 
+                case 3:
+                    Console.WriteLine("Multiplication : " + (x * y));
+                    break;
+                    case 4:
+                    Console.WriteLine("Division : " + (x / y));
+                    break;
+                    default:
+                    Console.WriteLine("Enter Correct Option");
+                    break;
+
+
+            }
+        }
+    }
+}
--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -6,13 +6,47 @@ namespace ProgrammingConstruct
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Basic Problems");
-            //Equality.CheckEquality();
-            //OddEven.CheckOddEven();
-            // Voter.CheckVoter();
-            //  LargestNumber.MaximumIntegerNumber(34, 12, 5);
-            // AdmissionEligibility.CheckEligibility();
-            WeekDays.Week();
+            Console.WriteLine("Basic Problems");
+            Console.WriteLine("Select given options");
+            Console.WriteLine("1.CheckEqua;ity\n2.OddEven\n3.Voting\n4.Large Number\n5.Eligibility For Admission\n6.DayWeek\n7.Arithmatic Operation\n8.Exit ");
+            int option = Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Check Equality of numbers");
+                    Equality.CheckEquality();
+                    break;
+                    case 2:
+                    Console.WriteLine("OddEven");
+                    break; 
+                case 3:
+                    Console.WriteLine("Check Age agg Voter");
+                    Voter.CheckVoter();
+                    break;
+                    case 4:
+                    Console.WriteLine("Largest number");
+                    LargestNumber.MaximumIntegerNumber(1, 3, 5);
+                    break;
+                    case 5:
+                    Console.WriteLine("Check Eligibility of Marks");
+                    AdmissionEligibility.CheckEligibility();
+                    break;
+                    case 6:
+                    Console.WriteLine("WeekDays");
+                    WeekDays.Week();
+                    break; 
+                case 7:
+                    Console.WriteLine("Arithmatic Operation");
+                    ArithmaticOperation.Calculator();
+                    break;
+                    default: Console.WriteLine("Enter Correct Number");
+                    break;
+
+
+
+
+
+            }
         }
     }
 }
